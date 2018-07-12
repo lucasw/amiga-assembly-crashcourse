938c80f6948e90710f696b3dbc23eed454b85869
Lucas Walter <wsacul@gmail.com>
2018-07-12 06:29:11 -0700

    rename source file

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 vasmm68k_mot -kick1hunks -Fhunkexe -o bpl -nosym bpl.s
 vasmm68k_mot -kick1hunks -Fhunkexe -o bpl_copper -nosym bpl_copper.s
-vasmm68k_mot -kick1hunks -Fhunkexe -o ex -nosym source.asm
+vasmm68k_mot -kick1hunks -Fhunkexe -o ex -nosym example.s
 
